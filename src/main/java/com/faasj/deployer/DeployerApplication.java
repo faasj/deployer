e28b5afe9ba862aa199a35922ef91326074dfb4f
Roman Ivanov <roivanoff@gmail.com>
2021-06-13 20:51:55 +0300

    first

---
+++ src/main/java/com/faasj/deployer/DeployerApplication.java
@@ -0,0 +1,13 @@
+package com.faasj.deployer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/faasj/deployer/DeployerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faasj.deployer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
